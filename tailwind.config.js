58fbd76c1175e259bf6c99434a396b67b272a9a2
hendro <hendro.tb@gmail.com>
2022-05-19 17:16:10 +0700

    tailwind config: setting font-family and extend fontSize base and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Overpass', 'sans-serif']
+    },
+    extend: {
+      fontSize:{
+        'base': '15px'
+      },
+      colors: {
+        'primary': 'hsl(25, 97%, 53%)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'medium-grey': 'hsl(216, 12%, 54%)',
+        'dark-blue': 'hsl(213, 19%, 18%)',
+        'very-dark-blue': 'hsl(216, 12%, 8%)'
+      }
+    },
   },
   plugins: [],
 }
