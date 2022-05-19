4328467fda6c1ee8791870136cd4b354ccdee44f
hendro <hendro.tb@gmail.com>
2022-05-19 19:35:37 +0700

    pages/rating: add page rating

--- src/pages/rating.vue
+++ src/pages/rating.vue
@@ -1,3 +1,25 @@
 <template>
-  rating
+  <div class="text-white">
+    <div class="mb-6">
+      <img src="../assets/icon-star.svg" alt="Star image" class="p-2.5 rounded-full w-8 bg-dark-blue ">
+    </div>
+
+    <h1 class="mb-3 text-xl font-bold">How did we do?</h1>
+
+    <p class="mb-3 text-xs text-medium-grey">
+      Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+    </p>
+
+    <div class="flex justify-between mb-6 text-sm text-center item-center text-medium-grey">
+      <div class="w-10 h-10 p-3 rounded-full bg-dark-blue">1</div>
+      <div class="w-10 h-10 p-3 rounded-full bg-dark-blue">2</div>
+      <div class="w-10 h-10 p-3 rounded-full bg-dark-blue">3</div>
+      <div class="w-10 h-10 p-3 rounded-full bg-dark-blue">4</div>
+      <div class="w-10 h-10 p-3 rounded-full bg-dark-blue">5</div>
+    </div>
+
+    <button class="w-full h-8 text-sm bg-primary rounded-2xl">
+      SUBMIT
+    </button>
+  </div>
 </template>
