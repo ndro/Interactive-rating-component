3052d2bdd8658b4b28ee43dc0386fee6afad8f58
hendro <hendro.tb@gmail.com>
2022-05-19 17:29:42 +0700

    src/App.vue: set template base

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <router-view />    
+  <div class="flex items-center justify-center min-h-screen font-sans antialiased bg-very-dark-blue">
+    <div class="shadow-lg rounded-2xl p-6 bg-dark-blue w-[320px] text-center">
+      <router-view />
+    </div>
+  </div>
+  <div class="flex items-center justify-center w-full h-10 text-slate-500">
+    Challenge by&nbsp;<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by&nbsp;<a href="https://github.com/ndro" target="_blank">ndro</a>.
+  </div>
 </template>
