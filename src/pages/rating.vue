b5eebd2afa0f10dcf55f6453db0f6328de7e4aca
hendro <hendro.tb@gmail.com>
2022-05-21 11:03:51 +0700

    pages/rating: add transition animation

--- src/pages/rating.vue
+++ src/pages/rating.vue
@@ -27,37 +27,37 @@ const submit = () => {
     <div class="flex justify-between mb-6 text-sm text-center item-center text-medium-grey">
       <div>
         <input name="rating" type="radio" id="1" value="1" v-model="ratingValue" class="hidden peer"/>
-        <label for="1" class="inline-block w-10 h-10 p-3 font-semibold rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
+        <label for="1" class="inline-block w-10 h-10 p-3 font-semibold transition duration-300 rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
           1
         </label>
       </div>
       <div>
         <input name="rating" type="radio" id="2" value="2" v-model="ratingValue" class="hidden peer"/>
-        <label for="2" class="inline-block w-10 h-10 p-3 font-semibold rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
+        <label for="2" class="inline-block w-10 h-10 p-3 font-semibold transition duration-300 rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
           2
         </label>
       </div>
       <div>
         <input name="rating" type="radio" id="3" value="3" v-model="ratingValue" class="hidden peer"/>
-        <label for="3" class="inline-block w-10 h-10 p-3 font-semibold rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
+        <label for="3" class="inline-block w-10 h-10 p-3 font-semibold transition duration-300 rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
           3
         </label>
       </div>
       <div>
         <input name="rating" type="radio" id="4" value="4" v-model="ratingValue" class="hidden peer"/>
-        <label for="4" class="inline-block w-10 h-10 p-3 font-semibold rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
+        <label for="4" class="inline-block w-10 h-10 p-3 font-semibold transition duration-300 rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
           4
         </label>
       </div>
       <div>
         <input name="rating" type="radio" id="5" value="5" v-model="ratingValue" class="hidden peer"/>
-        <label for="5" class="inline-block w-10 h-10 p-3 font-semibold rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
+        <label for="5" class="inline-block w-10 h-10 p-3 font-semibold transition duration-300 rounded-full cursor-pointer bg-dark-blue peer-hover:bg-light-grey peer-hover:text-white peer-checked:bg-primary peer-checked:text-white">
           5
         </label>
       </div>
     </div>
 
-    <button @click="submit" class="w-full h-8 bg-primary rounded-2xl hover:bg-white hover:text-primary">
+    <button @click="submit" class="w-full h-8 transition duration-300 bg-primary rounded-2xl hover:bg-white hover:text-primary">
       <span class="text-xs font-semibold tracking-widest">SUBMIT</span>
     </button>
   </div>
